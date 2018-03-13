61dd41ad078be163b959e15a4c8133f106e6572f
mathieuheyd <mathieu.heyd@gmail.com>
2018-03-13 17:28:31 +0000

    Basic implementation of MatchMaker

--- match-making/src/main/scala/MatchMaker.scala
+++ match-making/src/main/scala/MatchMaker.scala
@@ -2,10 +2,24 @@ package main.scala
 
 class MatchMaker {
 
-  val registeredPlayer = Seq()
+  val registeredPlayers = Seq()
 
   def registerPlayer(id: String) = {
+    registeredPlayers += id
+  }
 
+  def unregisterPlayer(id: String) = {
+    registerPlayer -= id
+  }
+
+  def newGame(): Option[(String, String)] = {
+    registerPlayer match {
+      case player1 :: player2 :: remaining => {
+        registeredPlayers = remaining
+        Some((player1, player2))
+      }
+      case _ => None
+    }
   }
 
 }
